b31fe0b6a0df2e3bd189f1820b93ee53be45f9c9
unknown <julianrodrigonavarro@gmail.com>
2024-08-24 12:47:45 -0300

    first

---
+++ Pedidos.js
@@ -0,0 +1,113 @@
+/*
+
+!    FALTANTES
+- 1 Beldent sandia
+- Beldent Negro
+- Halls rojo
+- Halls negro
+- Halls azul
+- Topline Negro
+- Café en saquito
+- Pizzitos
+- Chisitos comunes
+- alka caramelos duros verdes
+- Topolin
+- ring pop
+- Biyu
+- Fulbito
+- alikal
+- Alfajor Milka triple
+- Alfajor Milka triple oreo
+
+
+!   PEDIDO JAVI
+
+
+
+
+!   PEDIDO SEBA
+? Saladix Picantes son de seba
+-
+
+
+
+!    PEDIDO LUCKY
+? Me deben el monto 2 unidades de lucky strike coral de 12
+- 1 Lucky uva 20
+
+
+
+!   PEDIDO GEORGALOS
+- Mismo pedido de la semana pasada
+- Cubanito relleno de chocolate 40u x 10gr (no sé el precio)
+- 6 cajas bazooka menta
+- 12 Chocolate semiamargo suchard 100grs oferta
+- 60 Full mani 100grs
+- 10 cajas flynn paff tutti fruti
+- 1 Toddy cacao en polvo el de mayor gramaje que haya
+- 2 Chupetines Palito de la selva
+    ?↑↑ Pedido sin venir
+
+
+
+
+!   PEDIDO AL KIOSCO
+
+
+
+
+!   PEDIDO SATIVA
+
+
+
+
+!   PEDIDO COCA
+- 3 Coca 2,25L
+- 1 sprite 250ml
+- 1 coca 250ml
+
+
+!   PEDIDO ADALES
+- sprite 250ml
+- coca 250ml
+- 1 Manaos Naranja 500cc
+
+
+
+!   PEDIDO NORBERTO
+- 2 Mill
+- 2 Red Point ON
+- 1 Red point de 10
+- 1 Philips 20 ks
+- 2 chester 12 blue spin
+- 2 chester 20 blue spin ks
+- 2 Milenio Mentolado
+
+
+!   PEDIDO SILVIO
+- Auriculares
+- Capitan blanco
+- Capitan triple
+
+
+!   PEDIDO ANALGESICOS MATIAS
+- Rasta Blanco 
+
+!   PEDIDO ALBERTO
+- 1 Capitan blanco
+- 1 Capitan triple
+- 2 fulbito mani
+- 1 Guaymallen Triple Blanco
+- 1 Guaymallen Triple Negro
+- 2 Guaymallen Blanco
+- 2 Guaymallen Negro
+
+
+!   PEDIDO VERACRUZ
+- 4 Malta 170gr (paquete de $1399)
+
+
+
+
+*/
+
